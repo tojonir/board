9d1b9188d3e64c963a5f8edeebaeb32745463345
tojonir <tojonirinandrianarijaona@gmail.com>
2023-03-16 10:21:38 +0300

    fix redux setup

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,1 +1,14 @@
-export {};
+import { AnyAction } from "@reduxjs/toolkit";
+
+export const appReducer = (
+  state: string = "Welcome to react starter by tojonirina",
+  action: AnyAction
+) => {
+  switch (action.type) {
+    case "app":
+      return "welcome";
+
+    default:
+      return state;
+  }
+};
--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,2 +1,10 @@
-// store
-export {};
+import { appReducer } from "@redux/reducers";
+import { configureStore } from "@reduxjs/toolkit";
+
+const store = configureStore({
+  reducer: {
+    app: appReducer,
+  },
+});
+
+export default store;
