04c3c15e17884c68d3f6aebf16c65db0b8826f1a
tojonir <tojonirinandrianarijaona@gmail.com>
2023-03-16 21:10:21 +0300

    modul setup,fix cursor

---
+++ src/components/Modal.tsx
@@ -0,0 +1,25 @@
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FC, ReactNode } from "react";
+
+interface ModalProps {
+  children: ReactNode;
+  close: () => void;
+}
+
+const Modal: FC<ModalProps> = ({ children, close }) => {
+  return (
+    <div className="absolute top-0 left-0 backdrop-blur-sm bg-white/30 w-screen h-screen flex justify-center items-center">
+      <div
+        className="w-[30px] h-[30px] rounded-[50%] flex justify-center items-center bg-white absolute top-5 right-5"
+        onClick={() => close()}
+      >
+        <FontAwesomeIcon icon={solid("xmark")} className="" />
+      </div>
+
+      {children}
+    </div>
+  );
+};
+
+export default Modal;
--- src/containers/BoardCardItem.tsx
+++ src/containers/BoardCardItem.tsx
@@ -1,3 +1,6 @@
+import Modal from "@components/Modal";
+import { regular } from "@fortawesome/fontawesome-svg-core/import.macro";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { updateRowAction } from "@redux/actions";
 import { useAppDispatch } from "@utils/hooks";
 import { FC, useRef, useState } from "react";
@@ -18,6 +21,7 @@ const BoardCardItem: FC<BoardCardItemProps> = ({ id, title, status }) => {
     x: 0,
     y: 0,
   });
+  const [showModal, setShowModal] = useState<boolean>(false);
   const [canDrag, setCanDrag] = useState<boolean>(false);
   const dispatch = useAppDispatch();
 
@@ -25,18 +29,19 @@ const BoardCardItem: FC<BoardCardItemProps> = ({ id, title, status }) => {
     setMouseMove({ x: e.clientX, y: e.clientY });
     const item = cardRef.current;
     if (item && canDrag) {
+      item.style.cursor = "grabbing";
       item.style.position = "absolute";
       item.style.top = mouseMove.y - offset.y + "px";
       item.style.left = mouseMove.x - offset.x + "px";
     }
   };
   const mouseup = (e: any) => {
+    const item = cardRef.current;
     let doc = document.elementsFromPoint(e.clientX, e.clientY);
     doc = doc.filter((d) => d.id !== "" && d.id !== "root");
     if (doc[0] && doc[0].id !== "" && doc[0].id !== status) {
       dispatch(updateRowAction(id, { status: doc[0].id }));
     } else {
-      const item = cardRef.current;
       if (item) {
         item.style.position = "relative";
         item.style.top = "0px";
@@ -46,27 +51,41 @@ const BoardCardItem: FC<BoardCardItemProps> = ({ id, title, status }) => {
     setCanDrag(false);
   };
   return (
-    <div
-      ref={cardRef}
-      onMouseDown={(e) => {
-        setOfsset({
-          x: e.nativeEvent.offsetX,
-          y: e.nativeEvent.offsetY,
-        });
-        setCanDrag(true);
-      }}
-      onMouseUp={(e) => mouseup(e)}
-      className={`bg-white p-1 min-w-[230px] rounded-[3px] ${
-        canDrag && "shadow-md"
-      }`}
-      onDrag={(e) => {
-        e.preventDefault();
-      }}
-      onDragEnd={(e) => e.preventDefault()}
-      onDragStart={(e) => e.preventDefault()}
-    >
-      <div className="pointer-events-none">{title}</div>
-    </div>
+    <>
+      <div
+        ref={cardRef}
+        onMouseDown={(e) => {
+          setOfsset({
+            x: e.nativeEvent.offsetX,
+            y: e.nativeEvent.offsetY,
+          });
+          setCanDrag(true);
+        }}
+        onMouseUp={(e) => mouseup(e)}
+        className={`bg-white p-1 min-w-[230px] rounded-[3px] cursor-grab ${
+          canDrag && "shadow-md"
+        }`}
+        onDrag={(e) => {
+          e.preventDefault();
+        }}
+        onDragEnd={(e) => e.preventDefault()}
+        onDragStart={(e) => e.preventDefault()}
+      >
+        <div className="flex justify-between items-center group">
+          <span>{title}</span>
+          <FontAwesomeIcon
+            icon={regular("eye")}
+            className="text-gray-400 text-[14px] cursor-pointer hidden group-hover:block"
+            onClick={() => setShowModal(true)}
+          />
+        </div>
+      </div>
+      {showModal && (
+        <Modal close={() => setShowModal(false)}>
+          <div></div>
+        </Modal>
+      )}
+    </>
   );
 };
 
