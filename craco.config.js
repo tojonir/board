e4f5038d77c582ff2629bfa90f5183fe7bdd607c
tojonir <tojonirinandrianarijaona@gmail.com>
2023-03-16 09:34:22 +0300

    setup

---
+++ craco.config.js
@@ -0,0 +1,13 @@
+const path = require("path");
+module.exports = {
+  webpack: {
+    alias: {
+      "@assets": path.resolve(__dirname, "./src/assets/"),
+      "@components": path.resolve(__dirname, "./src/components/"),
+      "@containers": path.resolve(__dirname, "./src/containers/"),
+      "@pages": path.resolve(__dirname, "./src/pages/"),
+      "@utils": path.resolve(__dirname, "./src/utils/"),
+      "@redux": path.resolve(__dirname, "./src/redux/"),
+    },
+  },
+};
---
+++ src/utils/readme.md
@@ -0,0 +1,1 @@
+this is a utils folder
