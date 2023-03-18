89b7a64ddddfa421b8f5d8770d055e0a805cd358
tojonir <tojonirinandrianarijaona@gmail.com>
2023-03-18 16:08:15 +0300

    workspace done

---
+++ src/pages/Workspace.tsx
@@ -0,0 +1,35 @@
+import Input from "@components/Input";
+import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FC } from "react";
+import { Link } from "react-router-dom";
+
+const Workspace: FC = () => {
+  return (
+    <div className="w-screen h-screen overflow-hidden flex justify-center items-center bg-gray-200">
+      <div className="w-1/3 h-fit bg-white rounded-[3px] p-2 flex flex-col justify-between">
+        <div className="">
+          <p className="text-[25px]">Workspace</p>
+          <div className="flex items-center py-5">
+            <Input label="enter workspace" />
+            <button className="bg-blue-600 w-1/3 text-white px-2 py-2 rounded-[3px]">
+              start
+            </button>
+          </div>
+        </div>
+        <div className="flex justify-between items-center text-gray-400">
+          <div className="cursor-pointer">
+            <FontAwesomeIcon
+              icon={solid("sign-out-alt")}
+              className="rotate-180 mr-2"
+            />
+            <span>logout</span>
+          </div>
+          <Link to="/project">Personal space</Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Workspace;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,6 +2,7 @@ import Auth from "@pages/Auth";
 import Board from "@pages/Board";
 import Home from "@pages/Home";
 import Project from "@pages/Project";
+import Workspace from "@pages/Workspace";
 import { FC } from "react";
 import { Routes as Router, Route } from "react-router-dom";
 
@@ -12,6 +13,7 @@ const Routes: FC = () => {
       <Route path="/project" element={<Project />} />
       <Route path="/login" element={<Auth />} />
       <Route path="/signup" element={<Auth />} />
+      <Route path="/workspace" element={<Workspace />} />
       <Route path="/board" element={<Board />} />
     </Router>
   );
