c0909424025f093419f2e8c319d3498cc4b61f40
tojonir <tojonirinandrianarijaona@gmail.com>
2023-03-20 11:51:42 +0300

    route setup

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const { google } = require("./routes/google");
+const { github } = require("./routes/github");
 
 const app = express();
 const port = process.env.PORT || 4000;
 app.use(express.json());
 app.use(cors());
+
+app.use("/auth/google", google);
+app.use("/auth/github", github);
 
 app.listen(port, () => console.log(`server is running on port ${port}`));
